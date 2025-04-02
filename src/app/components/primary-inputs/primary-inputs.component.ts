4fe7a75cfd0230abc6cf09f686343c474516af5d
Isabelle Braz <pih_braz@hotmail.com>
2025-04-02 13:50:39 -0300

    tratamento de erros

--- src/app/components/primary-inputs/primary-inputs.component.ts
+++ src/app/components/primary-inputs/primary-inputs.component.ts
@@ -1,5 +1,5 @@
 import { Component, forwardRef, Input } from '@angular/core';
-import { ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 
 type InputTypes = "text" | "email" | "password"
 
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { DefautLoginLayoutComponent } from '../../components/defaut-login-layout/defaut-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputsComponent } from '../../components/primary-inputs/primary-inputs.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -11,13 +14,22 @@ import { PrimaryInputsComponent } from '../../components/primary-inputs/primary-
     ReactiveFormsModule,
     PrimaryInputsComponent
   ],
+  providers:[
+      LoginService
+
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm!: FormGroup;
 
-  constructor(){
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastService: ToastrService
+
+  ){
     this.loginForm = new FormGroup({
       email: new FormControl ('',[Validators.required]),
       password: new FormControl ('',[Validators.required, Validators.minLength(6)])
@@ -25,4 +37,17 @@ export class LoginComponent {
 
   }
 
-}
+  submit(){
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastService.success("Login efetetuado com sucesso!"),
+      error: () => this.toastService.error("Opss! Erro inesperado! Tente novamente.")
+    })
+      
+
+  }
+
+  navigate(){
+    this.router.navigate(["signup"])
+  }
+
+  }
